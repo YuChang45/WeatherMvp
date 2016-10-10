8840ad17a3564baaf4010bdaf1cf645556477ae7
yuchang <378434999@qq.com>
2016-10-10 11:46:37 +0800

    天气显示fragment

---
+++ app/src/main/java/android/yuchang/weathermvp/ui/main/WeatherFragment.java
@@ -0,0 +1,73 @@
+package android.yuchang.weathermvp.ui.main;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.yuchang.weathermvp.R;
+
+public class WeatherFragment extends Fragment {
+    private static final String CITY_NAME = "cityName";
+
+    private String cityName;
+
+    private OnFragmentInteractionListener mListener;
+
+    public WeatherFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static WeatherFragment newInstance(String cityName) {
+        WeatherFragment fragment = new WeatherFragment();
+        Bundle args = new Bundle();
+        args.putString(CITY_NAME, cityName);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            cityName = getArguments().getString(CITY_NAME);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
